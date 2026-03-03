b393d62cc6e2f21d8e2d53076248c4a6dbbf6329
Rezaul <mac@Rezauls-MacBook-Air.local>
2026-03-03 21:58:23 +0600

    first commit

---
+++ kotlinlib/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("maven-publish")
+}
+
+group = "com.github.DeveloperRejaul"
+version = "1.0.0"
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/DeveloperRejaul/kotlinlib")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
---
+++ kotlinlib/src/main/java/com/rezaul/kotlinlib/MyLib.kt
@@ -0,0 +1,7 @@
+package com.rezaul.kotlinlib
+
+object MyLib {
+    fun sayHello(name: String): String {
+        return "Hello $name! This is from my public Kotlin library."
+    }
+}
